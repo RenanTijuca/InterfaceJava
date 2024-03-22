22ddb1bb42aa0c1ea1078d61b931579c4dda54cf
Renan <125418452+RenanTijuca@users.noreply.github.com>
2024-03-21 22:08:28 -0300

    Add files via upload

---
+++ lpj-interface-usuario-master/src/main/java/com/unialfa/exerciciote/App.java
@@ -0,0 +1,13 @@
+package com.unialfa.exerciciote;
+
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() ->{
+            var calculadora = new Calculadora();
+            calculadora.setVisible(true);
+        });
+    }
+}
